53d0ef205c823163e4cb99a3620d58ac960d35e1
Yee Mon <thantyeemon275@gmail.com>
2024-05-08 15:20:11 +0630

    [complete Min Thein Kha]

--- YMDotNetCore.HttpClient/Program.cs
+++ YMDotNetCore.HttpClient/Program.cs
@@ -1,13 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
 Console.WriteLine("Hello, World!");
 string jsonStr = await File.ReadAllTextAsync("data.json");
+var model = JsonConvert.DeserializeObject<MainDto>(jsonStr);
+
 Console.WriteLine(jsonStr);
 Console.ReadLine();
 
 
 
 
-public class Rootobject
+public class MainDto
 {
     public Question[] questions { get; set; }
     public Answer[] answers { get; set; }
---
+++ YMDotNetCore.RestApiWithNLayer/Features/MinTheinKha/MinTheinKhaController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace YMDotNetCore.RestApiWithNLayer.Features.MinTheinKha
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MinTheinKhaController : ControllerBase
+    {
+        private async Task<LatHtaukBayDin> GetDataAsync()
+        {
+            string jsonStr = await System.IO.File.ReadAllTextAsync("data.json");
+            var model = JsonConvert.DeserializeObject<LatHtaukBayDin>(jsonStr);
+            return model;
+        }
+
+        [HttpGet("question")]
+        public async Task<IActionResult> Questions()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.questions);
+        }
+        [HttpGet]
+        public async Task<IActionResult> NumberList()
+        {
+            var model = await GetDataAsync();
+            return Ok(model.numberList);
+        }
+        [HttpGet("{questionNo}/{no}")]
+        public async Task<IActionResult> Answers(int questionNo,int no)
+        {
+            var model = await GetDataAsync();
+            return Ok(model.answers.FirstOrDefault(x=>x.questionNo == questionNo && x.answerNo==no));
+
+        }
+    }
+
+    public class LatHtaukBayDin
+    {
+        public Question[] questions { get; set; }
+        public Answer[] answers { get; set; }
+        public string[] numberList { get; set; }
+    }
+
+    public class Question
+    {
+        public int questionNo { get; set; }
+        public string questionName { get; set; }
+    }
+
+    public class Answer
+    {
+        public int questionNo { get; set; }
+        public int answerNo { get; set; }
+        public string answerResult { get; set; }
+    }
+
+
+}
